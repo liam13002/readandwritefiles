e95c0ca7f526003d9fd7ad1a64a00872ac47df31
liam13002 <liam_mccolgan1@baylor.edu>
2022-09-06 12:58:42 -0500

    completed customer_country

--- customer_country.py
+++ customer_country.py
@@ -2,14 +2,22 @@ import csv
 
 infile = open('customers.csv', 'r')
 
-outfile =open('customer_country.csv', 'w')
+csvfile =csv.reader(infile, delimiter=',')
 
-count = 0
+outfile = open("customer_country.csv", 'w')
 
-for line in infile:
-    outfile.write(line[1])
-   
-    count = count + 1
+next(csvfile)
 
-print(count)
+outfile.write("Full Name, Country\n")
+
+for record in csvfile:
+    
+    full_name = record[1] + " " + record[2]
+    country = record[4]
+
+    outfile.write(full_name + "," + country + "\n")
+
+outfile.close()
+
+
 
