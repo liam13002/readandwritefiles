80c54c871b612b02bea64d26a06731231f1887d5
liam13002 <liam_mccolgan1@baylor.edu>
2022-09-08 00:47:17 -0500

    finished avg steps

--- employee_bonus.py
+++ employee_bonus.py
@@ -12,12 +12,12 @@ for record in csvfile:
 
         pay = int(record[3])
         bonus = float(record[4])
-        total_pay = pay + (bonus*pay)
+        total_pay = format(pay + (bonus*pay), ',.2f')
 
         print("ID:", record[0])
         print("First name:", record[1])
         print("Last name:", record[2])
-        print("Salary:", record[3])
+        print("Salary: $", pay,  sep="")
         print("Bonus:", record[4])
-        print("Total Pay:", total_pay)
+        print("Total Pay: $", total_pay, sep="")
         input("Press enter for next customer:")
